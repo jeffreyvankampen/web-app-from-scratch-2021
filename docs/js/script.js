295720ae66c494167df3142dc831573d8582b667
jeffreyvankampen <vankampenjeffrey@gmail.com>
2021-02-15 17:28:34 +0100

    commit

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,70 +1,61 @@
-async function getData(value){
-  let url = `${endpoint}?apikey=${key}&s=star`;
-  
-  if (value) {
-    url = `${endpoint}?apikey=${key}&s=${value}`;
-  }
-    const endpoint = 'http://www.omdbapi.com/';
-    const key = '827f3e5d';
-    
-//getData met de input combineren 
-    fetch(url)
-      .then(response => response.json())
-    //   .then(data => clean(data.data))
-    //   .then(data => store(data))
-    .then(data => showData(data))
-      .catch(err => {
-        console.log(err);
-      });
-  }
-  
-  getData()
+const search = document.querySelector('#search')
+const button = document.querySelector('#button')
 
-  function showData(data) { // toont de data op de website
+function handleRoutes() {
+  routie({
+    '': () => {
+      getData()
+    },
+    ':id': id => {
+      getData(id)
+    }
+  });
+}
+
+// button.addEventListener('click', readInput)
+
+async function getData(value) {
+  const key = '827f3e5d';
+  const endpoint = 'http://www.omdbapi.com/';
+  let url = `${endpoint}?apikey=${key}&s=new`;
+
+  if (value) {
+    url = `${endpoint}?apikey=${key}&i=${value}`;
+  }
+  // getData met de input combineren
+
+  await fetch(url)
+    .then(response => response.json())
+    .then(data => showData(data))
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+function showData(data) { // toont de data op de website
+  const movieList = document.querySelector('.movieList');
+  movieList.innerHTML = '';
+  if (data.Search) { //Search is alleen beschikbaar met een lijst van meerdere zoekresultaten via de tag
     const movies = data.Search; // met de 'await' wacht het script tot de data geladen is
     console.log(movies); // kan je in de console kijken wat de mogelijk data is
-    const movieList = document.getElementById('movieList');
-
-  console.log(movieList)
     const movieArray = movies.map(movie => {
       return (
-        `<a href="#${movie.imdbID}"><article> <h2> ${movie.Title}</h2> <img src=${movie.Poster} /></article></a>`
-        )
+        `<a href="#${movie.imdbID}"><article><img src=${movie.Poster}/><h2> ${movie.Title}</h2></article></a>`
+      )
     })
-    const html = "<h2>TEST</h2>"
-    console.log(movieArray)
     movieList.insertAdjacentHTML('beforeend', movieArray.join(' '));
-  }   
+  } else { //Bij enkel resultaat voer de else functie uit
 
+    const movie = data
+    const movieDetail =
+      `<article><h2>Film ${movie.Title}</h2></article>`
+    movieList.insertAdjacentHTML('beforeend', movieDetail);
+    console.log(data)
+  }
+}
 
-  // Dynamische request doen
-  // const search = document.querySelector('search')
-  // const inputValue = search.value
-  // `${endpoint}?apikey=${key}&s=${inputValue}`
-  // 
+function readInput() {
+  inputValue = search.value
+}
 
-  // als je het formulier submit =>
-  // inputValue meegeven aan getData(inputvalue)
-
-//   const search = document.querySelector('#search')
-//   console.log(search)
-
-//   const button = document.querySelector('#button')
-//   console.log(button)
-
-//   let inputValue; 
-  
-//   function readInput() {
-//   inputValue = search.value
-//   console.log(inputValue)
-//   }
-
-//   button.addEventListener("click", readInput()) {
-//     console.log(search.value)
-// });
-
-
-
-
-
-
+handleRoutes()
